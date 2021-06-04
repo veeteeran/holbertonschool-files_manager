e8cf08c2c9f2182ac5c0ecff8c6c742fd518a709
StaciAF <aaen.it19@gmail.com>
2021-06-04 13:27:28 -0700

    adds solution for task 2

---
+++ utils/db.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient;
+class DBCLient {
+    constructor() {
+        const localHost = process.env.DB_HOST || 'localhost'
+        const port = process.env.DB_PORT || 27017
+        const database = process.env.DB_DATABASE || 'file_manager'
+        const url = `mongodb://${localHost}:${port}`
+        this.client =  new MongoClient(url, { useUnifiedTopology: true });
+        this.client.connect((err) => {
+            this.db = this.client.db(database)
+        })
+    }
+    isAlive() {
+        return this.client.isConnected()
+    }
+    async nbUsers() {
+        return this.db.collection('users').countDocuments({})
+    }
+    async nbFiles() {
+        return this.db.collection('files').countDocuments({})
+    }
+
+}
+
+const dbClient = new DBCLient();
+module.exports = dbClient
