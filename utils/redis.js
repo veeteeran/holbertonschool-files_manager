78415843fcab4cf9b89606b84a61a50f7ecac961
veeteeran <vietnamtran@gmail.com>
2021-06-04 12:45:15 -0700

    Create redis client

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import redis from "redis"
+import { promisify } from 'util'
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient()
+    this.client.on("error", error => {
+      console.log(`Redis client not connected to the server: ${error}`)
+    })
+  }
+
+  isAlive() {
+    return this.client.connected
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client)
+    return await getAsync(key).catch(console.error)
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client)
+    await setAsync(key, value, 'EX', duration).catch(console.error)
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client)
+    await delAsync(key).catch(console.error)
+  }
+}
+
+const redisClient = new RedisClient()
+
+export default redisClient
