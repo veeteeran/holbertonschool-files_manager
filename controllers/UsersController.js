61e28c7d9de8ad92a706b4122c241abc241b077f
veeteeran <vietnamtran@gmail.com>
2021-06-05 12:33:19 -0700

    Add try catch

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,30 +3,34 @@ const { createHash } = require('crypto')
 
 class UsersController {
   static async postNew(request, response) {
-    const { email, password } = request.body
+    try {
+      const { email, password } = request.body
 
-    if (!email) {
-      response.status(400)
-      response.json({ error: "Missing email" })
-    }
+      if (!email) {
+        response.status(400)
+        response.json({ error: "Missing email" })
+      }
 
-    if (!password) {
-      response.status(400)
-      response.json({ error: "Missing password" })
-    }
+      if (!password) {
+        response.status(400)
+        response.json({ error: "Missing password" })
+      }
 
-    const collection = dbClient.db.collection('users')
-    const cursor = collection.find({ email: email })
-    if ((await cursor.count()) > 0) {
-      response.status(400)
-      response.json({ error: "Already exists" })
-    } else {
-      const hash = createHash('sha1')
-      hash.update(password)
-      const user = await collection.insertOne({ email: email, password: hash.digest('hex') })
+      const collection = dbClient.db.collection('users')
+      const cursor = collection.findOne({ email: email })
+      if ((await cursor.count()) > 0) {
+        response.status(400)
+        response.json({ error: "Already exists" })
+      } else {
+        const hash = createHash('sha1')
+        hash.update(password)
+        const user = await collection.insertOne({ email: email, password: hash.digest('hex') })
 
-      response.status(201)
-      response.json({email: email, id: user.insertedId})
+        response.status(201)
+        response.json({ email: email, id: user.insertedId })
+      }
+    } catch (err) {
+      console.log(err)
     }
   }
 }
