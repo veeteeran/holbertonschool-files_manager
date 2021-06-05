35f99bfdf78635d0a390ab5ffee766168e3fca7e
veeteeran <vietnamtran@gmail.com>
2021-06-05 12:00:16 -0700

    Add post to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { getStats, getStatus } from '../controllers/AppController'
+import { postNew } from '../controllers/UsersController'
 
 const router = express.Router()
 
 router.get('/stats', getStats)
 router.get('/status', getStatus)
+router.use(express.json())
+router.post('/users', postNew)
 
 module.exports = router
