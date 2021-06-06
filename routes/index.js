ed13401fb2db5faedc710ca0a23c98333f2e7b91
veeteeran <vietnamtran@gmail.com>
2021-06-06 09:45:57 -0500

    Add getDisconnect

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import { getStats, getStatus } from '../controllers/AppController'
 import { postNew } from '../controllers/UsersController'
-import { getConnect } from '../controllers/AuthController'
+import { getConnect, getDisconnect } from '../controllers/AuthController'
 
 const router = express.Router()
 
 router.get('/stats', getStats)
 router.get('/status', getStatus)
 router.get('/connect', getConnect)
-// router.get('/disconnect', getDisconnect)
+router.get('/disconnect', getDisconnect)
 // router.get('/users/me', getMe)
 router.use(express.json())
 router.post('/users', postNew)
