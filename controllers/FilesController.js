e458487a441485c6ed886d9468de25040de58893
StaciAF <aaen.it19@gmail.com>
2021-06-07 10:51:39 -0700

    adds linter updates // isPublic is never reassigned error

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,55 +1,57 @@
-import dbClient from '../utils/db'
-import redisClient from '../utils/redis'
-import { v4 as uuidv4 } from 'uuid'
-import fs from 'fs'
-const mongo = require('mongodb')
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const mongo = require('mongodb');
 
 class FilesController {
   static async postUpload(request, response) {
     const {
       name,
       type,
-      data } = request.body
-    let {parentId, isPublic = false} = request.body
+      data,
+    } = request.body;
+    let { parentId, isPublic = false } = request.body;
 
-    if (!name) return response.status(400).json({ error: 'Missing name' })
+    if (!name) return response.status(400).json({ error: 'Missing name' });
 
-    if (!type) return response.status(400).json({ error: 'Missing type' })
+    if (!type) return response.status(400).json({ error: 'Missing type' });
 
-    if (!data && type !== 'folder') return response.status(400).json({ error: 'Missing data' })
+    if (!data && type !== 'folder') return response.status(400).json({ error: 'Missing data' });
 
     if (parentId) {
-      const o_id = new mongo.ObjectID(parentId)
-      const file = await dbClient.db.collection('files').findOne({ "_id": o_id })
+      const objectId = new mongo.ObjectID(parentId);
+      const file = await dbClient.db.collection('files').findOne({ _id: objectId });
 
-      if (!file) return response.status(400).json({ error: 'Parent not found' })
+      if (!file) return response.status(400).json({ error: 'Parent not found' });
 
-      if (file && file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' })
+      if (file && file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' });
     } else {
-      parentId = 0
+      parentId = 0;
     }
 
-    let newFile
-    const token = request.headers['x-token']
-    const id = await redisClient.get(`auth_${token}`)
-    if (parentId !== 0) parentId = new mongo.ObjectID(parentId)
+    let newFile;
+    const token = request.headers['x-token'];
+    const id = await redisClient.get(`auth_${token}`);
+    if (parentId !== 0) parentId = new mongo.ObjectID(parentId);
     if (type === 'folder') {
       newFile = await dbClient.db.collection('files').insertOne({
         userId: new mongo.ObjectID(id),
         name,
         type,
         isPublic,
-        parentId
-      })
+        parentId,
+      });
     } else {
-      const path = process.env.FOLDER_PATH || '/tmp/files_manager'
-      if (!fs.existsSync(path)) fs.mkdirSync(path)
+      const path = process.env.FOLDER_PATH || '/tmp/files_manager';
+      if (!fs.existsSync(path)) fs.mkdirSync(path);
 
-      const uuid = uuidv4()
-      const localPath = `${path}/${uuid}`
-      const inputData = Buffer.from(data, 'base64').toString()
+      const uuid = uuidv4();
+      const localPath = `${path}/${uuid}`;
+      const inputData = Buffer.from(data, 'base64').toString();
 
-      await fs.promises.writeFile(localPath, inputData)
+      await fs.promises.writeFile(localPath, inputData);
 
       newFile = await dbClient.db.collection('files').insertOne({
         userId: new mongo.ObjectID(id),
@@ -57,11 +59,13 @@ class FilesController {
         type,
         isPublic,
         parentId,
-        localPath
-      })
+        localPath,
+      });
     }
-    return response.status(201).json({id: newFile.insertedId, userId: id, name, type, isPublic, parentId})
+    return response.status(201).json({
+      id: newFile.insertedId, userId: id, name, type, isPublic, parentId,
+    });
   }
 }
 
-module.exports = FilesController
+module.exports = FilesController;
