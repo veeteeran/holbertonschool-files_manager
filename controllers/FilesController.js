d28ec158c524ccfb559007de6d18fffa994b6460
veeteeran <vietnamtran@gmail.com>
2021-06-10 15:16:33 -0700

    Add mime import

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import mime from 'mime-types';
 
 const mongo = require('mongodb');
 
